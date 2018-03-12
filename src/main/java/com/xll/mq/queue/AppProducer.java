0e804ce5f8038c7bc9f5c94e421a165c50b83c28
XieLulin <fengshiyeyileng@163.com>
2018-03-12 21:08:27 +0800

    first commit

---
+++ src/main/java/com/xll/mq/queue/AppProducer.java
@@ -0,0 +1,12 @@
+package com.xll.mq.queue;
+
+
+ /** 
+ * @ClassName: AppProducer 
+ * @Description: 消息发送者
+ * @author: XieLulin
+ * @date: 2018年3月12日 下午9:03:16  
+ */
+public class AppProducer {
+
+}
